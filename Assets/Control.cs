24fc07cfce4eb1f6e45a1febadcf71a668dd4b29
Luis López <115268415+luislopez229@users.noreply.github.com>
2024-11-25 17:54:25 +0100

    Update Control.cs

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -6,9 +6,39 @@ public class Control : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool impulsandose;
+    bool paused = false;
     void Start()
     {
         impulsandose = false;
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            paused = togglePause();
+    }
+
+    void OnGUI()
+    {
+        if (paused)
+        {
+            GUILayout.Label("Game is paused!");
+            if (GUILayout.Button("Click me to unpause"))
+                paused = togglePause();
+        }
+    }
+
+    bool togglePause()
+    {
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+            return (false);
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            return (true);
+        }
+    }
 }
