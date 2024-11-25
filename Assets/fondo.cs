c883dda679d2d691d0a2f1b119179cdd1b56b243
Luis López <115268415+luislopez229@users.noreply.github.com>
2024-11-25 18:07:52 +0100

    Update fondo.cs

--- Assets/fondo.cs
+++ Assets/fondo.cs
@@ -39,12 +39,12 @@ void Update()
     void Update()
     {
         if(!prueba){
-        target = new Vector3(cam.transform.position.x + 20,cam.transform.position.y + 20,0);
+        target = new Vector3(cam.transform.position.x + 30,cam.transform.position.y + 30,0);
         prueba = true;
         }
         transform.position = Vector2.MoveTowards(transform.position, target, 0.001f);
     if (transform.position == target){
-        transform.position = -target;
+        transform.position = new Vector3(cam.transform.position.x - 20, cam.transform.position.y - 20, 0);
         prueba = false;
     }
     }
